69a03caf23fd1848e56fa18a360e48b17220c1df
Toxic_Blade <toxicblade.work@gmail.com>
2023-11-25 12:33:01 +0200

    git ignore changed, reqs added,readme updated

--- main.py
+++ main.py
@@ -2,8 +2,78 @@ import numpy as np
 import seaborn as sns
 import pandas as pd
 
-from header_analyser import get_header_type
+import sklearn
+import joblib
 
-DATABASE = "a.csv"
+from flask import Flask, jsonify
+from flask_cors import CORS
+from flask import Flask, jsonify, request
 
-print(get_header_type(DATABASE))
+app = Flask(__name__)
+CORS(app)  # Enable CORS for your Flask app
+
+
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    # Здесь можно вернуть данные, которые клиент запросил
+    data = {'message': 'Idi nahui'}
+    return jsonify(data)
+
+
+@app.route('/api/request', methods=['POST'])
+def handle_request():
+    try:
+        model = joblib.load('children_violance_predictor.joblib')
+        # Retrieve the JSON data sent from the form
+        data = request.json
+
+        gender_value = data.get('dropdown1', None)
+        gender = 0 if gender_value == 'Female' else 1 if gender_value == 'Male' else None
+        # Extract the specific fields from the data
+        age = data.get('additionalField', None)
+        mother = 1 if data.get('checkbox1', False) else 0
+        father = 1 if data.get('checkbox2', False) else 0
+        smoke = 1 if data.get('checkbox3', False) else 0
+        drinks = 1 if data.get('checkbox4', False) else 0
+        drugs = 1 if data.get('checkbox5', False) else 0
+
+
+        welfare = data.get('dropdown2', None)
+        welfare_L = 1 if welfare == 'low' else 0
+        welfare_M = 1 if welfare == 'medium' else 0
+
+
+        model_input = [age, mother, father, smoke, drugs, drinks, gender, welfare_L, welfare_M]
+
+        # Make the prediction
+        prediction = model.predict([model_input])
+
+        # Convert NumPy data types to Python data types for JSON serialization
+        prediction = prediction[0].item() if len(prediction) > 0 else None
+
+        # Return the prediction in the response
+        result = {
+            'prediction': prediction
+        }
+
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+
+@app.route('/api/csvdata', methods=['GET'])
+def get_csv_data():
+    try:
+        # Read the CSV file
+        df = pd.read_csv('./data_set.csv')
+        # Convert DataFrame to a list of dictionaries
+        data = df.to_dict(orient='records')
+        # Use jsonify to return the data
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
